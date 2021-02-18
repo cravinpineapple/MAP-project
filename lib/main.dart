66fd827eb5b77819e66401e497c0330e789efc1f
Noah Stinson <noahstjohn@ymail.com>
2021-02-18 17:12:11 -0600

    11 finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lesson3part1/screen/signin_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Lesson3Part1App());
 }
 
--- lib/screen/signin_screen.dart
+++ lib/screen/signin_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lesson3part1/controller/firebasecontroller.dart';
 
 class SignInScreen extends StatefulWidget {
   static const routeName = '/signInScreen';
@@ -10,6 +12,7 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInState extends State<SignInScreen> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   _Controller con;
 
   @override
@@ -26,7 +29,40 @@ class _SignInState extends State<SignInScreen> {
       appBar: AppBar(
         title: Text('Sign In Screen'),
       ),
-      body: Text('Sign In'),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                validator: con.validateEmail,
+                onSaved: con.saveEmail,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+                autocorrect: false,
+                obscureText: true,
+                validator: con.validatePassword,
+                onSaved: con.savePassword,
+              ),
+              RaisedButton(
+                onPressed: con.signIn,
+                child: Text(
+                  'Sign In',
+                  style: Theme.of(context).textTheme.button,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -34,4 +70,44 @@ class _SignInState extends State<SignInScreen> {
 class _Controller {
   _SignInState state;
   _Controller(this.state);
+
+  String email;
+  String password;
+
+  String validatePassword(String value) {
+    if (value.length < 6)
+      return 'too short';
+    else
+      return null;
+  }
+
+  void savePassword(String value) {
+    password = value;
+  }
+
+  String validateEmail(String value) {
+    if (value.contains('@') && value.contains('.'))
+      return null;
+    else
+      return 'invalid email address';
+  }
+
+  void saveEmail(String value) {
+    email = value;
+  }
+
+  void signIn() async {
+    if (!state.formKey.currentState.validate()) return;
+
+    // now validated
+    state.formKey.currentState.save();
+
+    User user;
+    try {
+      user = await FirebaseController.signIn(email: email, password: password);
+      print('============ ${user.email}');
+    } catch (e) {
+      print('============ $e');
+    }
+  }
 }
