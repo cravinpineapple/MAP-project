82bd54cfe9678500e6499fa64893fe8d71f57966
Noah Stinson <noahstjohn@ymail.com>
2021-03-27 13:23:03 -0500

    formatting

--- lib/screen/signin_screen.dart
+++ lib/screen/signin_screen.dart
@@ -141,8 +141,8 @@ class _Controller {
       user = await FirebaseController.signIn(email: email, password: password);
       print('============ ${user.email}');
     } catch (e) {
-      MyDialog.circularProgressStop(
-          state.context); // must make sure to stop load symbol in all possible paths
+      MyDialog.circularProgressStop(state
+          .context); // must make sure to stop load symbol in all possible paths
       MyDialog.info(
         context: state.context,
         title: 'Sign In Error',
