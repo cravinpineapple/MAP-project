b6a29fd9962c42045630f5405582f5f375977dfa
Jesse Rankins <jwrankins@gmail.com>
2021-04-07 18:58:30 -0500

    parsing into individual user record details

--- lib/model/comment.dart
+++ lib/model/comment.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/cupertino.dart';
-import 'package:lesson3part1/model/userrecord.dart';
 
 class Comment {
   String docID;
-  UserRecord commentOwner;
+  String commentOwnerEmail;
+  String username;
+  String profilePicURL;
   String message;
   DateTime datePosted;
 
-  static const COMMENT_OWNER = 'commentOwner';
   static const COMMENT_MESSAGE = 'commentMessage';
   static const DATE_POSTED = 'datePosted';
+  static const PROFILE_PIC_URL = 'profilePicURL';
+  static const USERNAME = 'username';
+  static const COMMENT_OWNER_EMAIL = 'commentOwnerEmail';
 
   Comment({
-    @required this.commentOwner,
+    @required this.commentOwnerEmail,
+    @required this.profilePicURL,
+    @required this.username,
     @required this.message,
     @required this.datePosted,
-    @required this.docID,
+    this.docID,
   });
 
   Map<String, dynamic> serialize() {
     return {
+      COMMENT_OWNER_EMAIL: this.commentOwnerEmail,
+      USERNAME: this.username,
+      PROFILE_PIC_URL: this.profilePicURL,
       COMMENT_MESSAGE: this.message,
-      COMMENT_OWNER: this.commentOwner,
       DATE_POSTED: this.datePosted,
     };
   }
 
   static Comment deserialize(Map<String, dynamic> doc, String docID) {
     return Comment(
-      commentOwner: doc[COMMENT_OWNER],
+      profilePicURL: doc[PROFILE_PIC_URL],
+      commentOwnerEmail: doc[COMMENT_OWNER_EMAIL],
+      username: doc[USERNAME],
       message: doc[COMMENT_MESSAGE],
-      datePosted: doc[DATE_POSTED],
+      datePosted: doc[DATE_POSTED] == null
+          ? null
+          : DateTime.fromMillisecondsSinceEpoch(doc[DATE_POSTED].millisecondsSinceEpoch),
       docID: docID,
     );
   }
